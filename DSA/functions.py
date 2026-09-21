3b6cea4528c7e2ab2c4364406317ce1c8219b0fd
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2026-09-21 09:05:46 +0530

    functions programms [1 to 6]

---
+++ DSA/functions.py
@@ -0,0 +1,42 @@
+# print("\n1. Write a function to print 'Hello World'\n")
+# def hello():
+#     print("Hello WOrld")
+# hello()
+
+# print("\n2. Write a function that takes a name and prints a greeting\n")
+# def greet(name):
+#     print("Hello...", name, "Har Har Mahadev")
+# greet("Nikhil")
+
+# print("\n3. Write a function to add two number\n")
+# def add(a, b):
+#     return a+b
+# result = add(101, 201)
+# print("Sum = ", result)
+
+# print("\n4. Write a function to find the square of a number\n")
+# def square(num):
+#     return num * num
+
+# result = square(5)
+# print("Square =", result)
+
+# print("\n5. Write a function to check whether a number is even or odd\n")
+# def check_even_odd(num):
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# num = int(input("Enter a number: "))
+# check_even_odd(num)
+
+# print("\n6. Write a function to find the maximum of two numbers\n")
+# def find_max(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+# print("Maximum =", find_max(num1, num2))
